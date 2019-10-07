98709f7447aa061f7b151cdfd3cda51a5b954f28
sxd <623048568@qq.com>
2019-10-07 12:06:05 +0800

    first commit

---
+++ src/main/java/com/itheima/ssm/service/impl/RoleServiceImpl.java
@@ -0,0 +1,51 @@
+package com.itheima.ssm.service.impl;
+
+import com.itheima.ssm.dao.IRoleDao;
+import com.itheima.ssm.domain.Permission;
+import com.itheima.ssm.domain.Role;
+import com.itheima.ssm.service.IRoleService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+@Transactional
+public class RoleServiceImpl implements IRoleService {
+
+    @Autowired
+    private IRoleDao roleDao;
+
+
+    public void deleteRoleById(String roleId) {
+        //从user_role表中删除
+        roleDao.deleteFromUser_RoleByRoleId(roleId);
+        //从role_permission表中删除
+        roleDao.deleteFromRole_PermissionByRoleId(roleId);
+        //从role表中删除
+        roleDao.deleteRoleById(roleId);
+    }
+
+    public void addPermissionToRole(String roleId, String[] permissionIds) {
+        for(String permissionId:permissionIds){
+            roleDao.addPermissionToRole(roleId,permissionId);
+        }
+    }
+
+    public Role findById(String roleId) {
+        return roleDao.findById(roleId);
+    }
+
+    public List<Permission> findOtherPermissions(String roleId) {
+        return roleDao.findOtherPermissions(roleId);
+    }
+
+    public void save(Role role) {
+        roleDao.save(role);
+    }
+
+    public List<Role> findAll() throws Exception{
+        return roleDao.findAll();
+    }
+}
